9f6eec781c3352bf03f99032b2d666ee989295d4
nalankrishnav <nalankrishnav@gmail.com>
2025-11-08 19:41:47 +0530

    first commit

---
+++ src/main/java/com/queuectlnk/queuectlnk/App.java
@@ -0,0 +1,10 @@
+package com.queuectlnk.queuectlnk;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
